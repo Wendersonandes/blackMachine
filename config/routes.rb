618f82ed68566166a5579e57f049baa76c39c98c
Wendersonandes <oldmaverick@gmail.com>
2017-09-01 15:35:47 -0300

    Added user resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,13 @@
 Rails.application.routes.draw do
-	mount Admin::Engine, at: "admin", :as => "admin"
-	devise_for :users, controllers: {
-		:registrations => 'users/registrations'
-	}  
-
-  devise_scope :user do
-		unauthenticated do
-			root 'users/registrations#new'
-		end
-		authenticated do
-			root :to => 'pages#home', as: :authenticated_root
-		end
-	end
+	mount Admin::Engine, :at => "admin", :as => "admin"
 
 	#Shrine uploader endpoints
   mount FileUploader::UploadEndpoint, at: "/attachments/file"
 	mount FeaturedImageUploader::UploadEndpoint, :at => "/attachments/featured_image"
 	mount ImageUploader::UploadEndpoint, :at => "/attachments/image"
 
-  resources :contacts, only: [:new, :create]
+	resources :users, :only => [:create]
+  resources :contacts, :only => [:new, :create]
 	resources :galleries do
 		resources :images
 	end
@@ -26,10 +15,11 @@ Rails.application.routes.draw do
 	resources :images
 
 	# Static pages
-  match '/error' => 'pages#error', via: [:get, :post], as: 'error_page'
-  get '/about' => 'pages#about', as: 'about'
-  get '/contact' => 'pages#contact', as: 'contact'
-  get '/home' => 'pages#home', as: 'home'
-  get '/downloads' => 'pages#downloads', as: 'downloads'
-  get '/events' => 'pages#events', as: 'events'
+  match '/error' => 'pages#error', :via => [:get, :post], :as => 'error_page'
+  get '/about' => 'pages#about', :as => 'about'
+  get '/contact' => 'pages#contact', :as => 'contact'
+  get '/home' => 'pages#home', :as => 'home'
+  get '/downloads' => 'pages#downloads', :as => 'downloads'
+  get '/events' => 'pages#events', :as => 'events'
+	root :to => 'pages#home'
 end
