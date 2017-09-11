44c99385df43bd19e76e46cc72b44e6c82d423db
Wendersonandes <oldmaverick@gmail.com>
2017-09-11 11:57:47 -0300

    Configured devise admin_users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  resources :settings
+  resources :site_names
 	mount Admin::Engine, :at => "admin", :as => "admin"
+
+	devise_for :admin_users, :controllers => { :sessions => 'admin_users/sessions', :registrations => 'admin_users/registrations', :passwords => 'admin_users/passwords'}
 
 	#Shrine uploader endpoints
   mount FileUploader::UploadEndpoint, at: "/attachments/file"
@@ -21,5 +25,6 @@ Rails.application.routes.draw do
   get '/home' => 'pages#home', :as => 'home'
   get '/downloads' => 'pages#downloads', :as => 'downloads'
   get '/events' => 'pages#events', :as => 'events'
+  get '/media' => 'pages#media', :as => 'media'
 	root :to => 'pages#home'
 end
