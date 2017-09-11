75ea5aa6ff2411f54c1b46610b7d3172c9236a39
Wendersonandes <oldmaverick@gmail.com>
2017-09-11 12:00:21 -0300

    Working on galleries

--- admin/app/controllers/admin/galleries_controller.rb
+++ admin/app/controllers/admin/galleries_controller.rb
@@ -3,5 +3,19 @@ require_dependency "admin/application_controller"
 module Admin
   class GalleriesController < ApplicationController
     include Godmin::Resources::ResourceController
+
+		def create
+			submit_status = params[:commit].parameterize.underscore
+			@gallery = Gallery.new(gallery_params)
+			if submit_status.include?('save_and_edit')
+				if @gallery.save
+					redirect_to edit_gallery_path(@gallery)
+				end
+			end
+		end
+
+		def gallery_params
+			params.require(:gallery).permit(:title, :description)
+		end
   end
 end
---
+++ app/cells/galleries/image_cell.rb
@@ -0,0 +1,7 @@
+module Galleries
+	class ImageCell < Cell::ViewModel
+		property :title
+		property :description
+		property :file_url
+	end
+end
