505574e2811fc371c7d7cbbe92c98b582677c1f7
Wendersonandes <oldmaverick@gmail.com>
2017-09-11 12:14:11 -0300

    Added quick forms to welcome dashboard view

--- admin/app/controllers/admin/application_controller.rb
+++ admin/app/controllers/admin/application_controller.rb
@@ -1,6 +1,24 @@
 module Admin
   class ApplicationController < ActionController::Base
     include Godmin::ApplicationController
+		include Godmin::Authentication
     protect_from_forgery with: :exception
+
+		def welcome
+			@event = Event.new
+			@gallery = Gallery.new
+		end
+
+		def authenticate_admin_user
+			authenticate_admin_user!
+		end
+
+		def admin_user
+			current_admin_user
+		end
+
+		def admin_user_signed_in?
+			admin_user_signed_in?
+		end
   end
 end
